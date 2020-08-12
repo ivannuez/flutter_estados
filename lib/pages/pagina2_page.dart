af15816961aa6605d3b17cff29b11a13402a1134
Fernando Herrera <fernando.herrera85@gmail.com>
2020-08-12 11:41:56 -0400

    Fin rama bloc

--- lib/pages/pagina2_page.dart
+++ lib/pages/pagina2_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:estados/models/usuario.dart';
+import 'package:estados/bloc/usuario/usuario_bloc.dart';
 
 
 class Pagina2Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    // ignore: close_sinks
+    final usuarioBloc = BlocProvider.of<UsuarioBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Pagina 2'),
@@ -19,6 +27,16 @@ class Pagina2Page extends StatelessWidget {
               color: Colors.blue,
               onPressed: () {
 
+                final newUser = new Usuario(
+                  nombre: 'Fernando Herrera',
+                  edad: 34,
+                  profesiones: ['Fullstack Developer']
+                );
+
+                usuarioBloc.add(
+                  ActivarUsuario(newUser)
+                );
+
               }
             ),
 
@@ -27,6 +45,8 @@ class Pagina2Page extends StatelessWidget {
               color: Colors.blue,
               onPressed: () {
 
+                usuarioBloc.add( CambiarEdad(30) );
+
               }
             ),
 
@@ -34,6 +54,8 @@ class Pagina2Page extends StatelessWidget {
               child: Text('Añadir Profesion', style: TextStyle( color: Colors.white ) ),
               color: Colors.blue,
               onPressed: () {
+
+                  usuarioBloc.add( AgregarProfesion('Nueva Profesion') );
 
               }
             ),
